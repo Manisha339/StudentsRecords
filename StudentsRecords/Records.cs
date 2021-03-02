c589490874d88c14204cbb5887e20e61836603f3
mallu <mallu@HSC-5YQM6Y2>
2021-03-02 19:53:22 +0530

    commit

--- StudentsRecords/Records.cs
+++ StudentsRecords/Records.cs
@@ -17,7 +17,7 @@ namespace StudentsRecords
             string dir = Directory.GetCurrentDirectory();
             string filename = dir + "\\RecordsOfStudent1.txt";
             Console.WriteLine(filename);
-            if ((File.Exists(filename)))
+            if (!(File.Exists(filename)))
             {
                 Console.WriteLine("File doesn't exist");
             }
@@ -25,7 +25,9 @@ namespace StudentsRecords
             {
                 Console.WriteLine("File exists");
                 String file1 = "Manisha, 21, IT";
+                String file2 = "Manisha, 21, IT";
                 File.WriteAllText(filename, file1);
+                File.WriteAllText(filename, file2);
                 string[] contents = File.ReadAllLines(filename);
                 foreach (var content in contents)
                 {
